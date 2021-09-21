f39bb83b816b33d01c59eeb74a131cc8c7afca9e
Mickael <40719970+Starius-Project@users.noreply.github.com>
2021-09-21 22:20:40 +0200

    add delay

--- main.ts
+++ main.ts
@@ -13,13 +13,17 @@ radio.onReceivedString(function (receivedString) {
     }
 })
 input.onButtonPressed(Button.B, function () {
-    servos.P0.setAngle(20)
+    servos.P0.setAngle(15)
+    basic.pause(100)
+    servos.P0.stop()
 })
 let InData = ""
 let released = 0
 radio.setGroup(1)
 radio.setTransmitPower(7)
-servos.P0.setAngle(20)
+servos.P0.setAngle(15)
+basic.pause(100)
+servos.P0.stop()
 basic.forever(function () {
     if (released == 0) {
         radio.sendString("1")
